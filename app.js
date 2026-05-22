71a14ba7a81da6024921822fa6a873f3ff7b5d69
Roadora <stephank21@hotmail.com>
2026-05-22 16:34:24 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".bottom-nav-item");
-
-  buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      buttons.forEach(btn => btn.classList.remove("active"));
-      button.classList.add("active");
-    });
-  });
+  console.log("Roadora Overzicht Clean Reset actief");
 });
