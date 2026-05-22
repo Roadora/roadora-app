ec291203be93e67dc5d51e7f0e6d5b8479856241
Roadora <stephank21@hotmail.com>
2026-05-22 14:29:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('.bottom-nav-item').forEach(button => {
-    button.addEventListener('click', () => {
-      document.querySelectorAll('.bottom-nav-item').forEach(btn => {
-        btn.classList.remove('active');
-      });
-      button.classList.add('active');
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".bottom-nav-item").forEach((button) => {
+    button.addEventListener("click", () => {
+      document.querySelectorAll(".bottom-nav-item").forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
     });
   });
 });
